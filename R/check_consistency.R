acb286baffbc1ede899ef335f7a19c26ace84fb2
adimajo <ztech1.25@gmail.com>
2020-10-14 16:00:01 +0200

    adressed CRAN comments

--- R/check_consistency.R
+++ R/check_consistency.R
@@ -1,23 +1,21 @@
-#' Check consistency for reject inference inputs
-#'
-#' This function checks consistency for reject inference inputs
-#' @param xf The matrix of financed clients' characteristics to be used in the scorecard.
-#' @param xnf The matrix of not financed clients' characteristics to be used in the scorecard (must be the same in the same order as xf!).
-#' @param yf The matrix of financed clients' labels
-#' @author Adrien Ehrhardt
-#' @seealso \code{glm}, \code{speedglm}
-#' @keywords internal
-#' @author Adrien Ehrhardt
-#' @examples
-#' \dontrun{
-#' # We simulate data from financed clients
-#' xf <- matrix(runif(100 * 2), nrow = 100, ncol = 2)
-#' theta <- c(2, -2)
-#' log_odd <- apply(xf, 1, function(row) theta %*% row)
-#' yf <- rbinom(100, 1, 1 / (1 + exp(-log_odd)))
-#' xnf <- matrix(runif(100 * 2), nrow = 100, ncol = 2)
-#' check_consistency(xf, xnf, yf)
-#' }
+# #' Check consistency for reject inference inputs
+# #'
+# #' This function checks consistency for reject inference inputs
+# #' @param xf The matrix of financed clients' characteristics to be used in the scorecard.
+# #' @param xnf The matrix of not financed clients' characteristics to be used in the scorecard (must be the same in the same order as xf!).
+# #' @param yf The matrix of financed clients' labels
+# #' @author Adrien Ehrhardt
+# #' @seealso \code{glm}, \code{speedglm}
+# #' @keywords internal
+# #' @author Adrien Ehrhardt
+# #' @examples
+# #' # We simulate data from financed clients
+# #' xf <- matrix(runif(100 * 2), nrow = 100, ncol = 2)
+# #' theta <- c(2, -2)
+# #' log_odd <- apply(xf, 1, function(row) theta %*% row)
+# #' yf <- rbinom(100, 1, 1 / (1 + exp(-log_odd)))
+# #' xnf <- matrix(runif(100 * 2), nrow = 100, ncol = 2)
+# #' check_consistency(xf, xnf, yf)
 check_consistency <- function(xf, xnf, yf) {
   for (obj in list(xf, xnf)) {
     if (!any(class(obj) %in% c("matrix", "array", "data.frame"))) {
--- R/model_f.R
+++ R/model_f.R
@@ -1,23 +1,22 @@
-#' Financed model
-#'
-#' This function learns a logistic regression on financed clients only
-#' @param df_f data frame of labels and financed clients' data
-#' @return Logistic regression model
-#' @author Adrien Ehrhardt
-#' @seealso \code{glm}, \code{speedglm}
-#' @references
-#' Enea, M. (2015), speedglm: Fitting Linear and Generalized Linear Models to Large Data Sets, \url{https://CRAN.R-project.org/package=speedglm}
-#' Ehrhardt, A., Biernacki, C., Vandewalle, V., Heinrich, P. and Beben, S. (2018), Reject Inference Methods in Credit Scoring: a rational review,
-#' @examples
-#' \dontrun{
-#' # We simulate data from financed clients
-#' xf <- matrix(runif(100 * 2), nrow = 100, ncol = 2)
-#' theta <- c(2, -2)
-#' log_odd <- apply(xf, 1, function(row) theta %*% row)
-#' yf <- rbinom(100, 1, 1 / (1 + exp(-log_odd)))
-#' df_f <- data.frame(labels = yf, x = xf)
-#' model_finances(df_f)
-#' }
+# #' Financed model
+# #'
+# #' This function learns a logistic regression on financed clients only
+# #' @param df_f data frame of labels and financed clients' data
+# #' @return Logistic regression model
+# #' @author Adrien Ehrhardt
+# #' @seealso \code{glm}, \code{speedglm}
+# #' @references
+# #' Enea, M. (2015), speedglm: Fitting Linear and Generalized Linear Models to Large Data Sets, \url{https://CRAN.R-project.org/package=speedglm}
+# #'
+# #' Ehrhardt, A., Biernacki, C., Vandewalle, V., Heinrich, P. and Beben, S. (2018), Reject Inference Methods in Credit Scoring: a rational review,
+# #' @examples
+# #' # We simulate data from financed clients
+# #' xf <- matrix(runif(100 * 2), nrow = 100, ncol = 2)
+# #' theta <- c(2, -2)
+# #' log_odd <- apply(xf, 1, function(row) theta %*% row)
+# #' yf <- rbinom(100, 1, 1 / (1 + exp(-log_odd)))
+# #' df_f <- data.frame(labels = yf, x = xf)
+# #' model_finances(df_f)
 model_finances <- function(df_f) {
   if (!(is_speedglm_installed() & is_speedglm_predict_installed())) {
     warning("Speedglm not installed, using glm instead (slower).", call. = FALSE)
